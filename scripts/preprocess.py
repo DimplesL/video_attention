ed7b1ea8a67febb978e6a2b9f1bcb4765fa02c44
Blaine Rister <blaine@ip-10-229-16-176.us-west-1.compute.internal>
2016-03-06 09:02:25 +0000

    add the new version of preprocess for word-level models

--- scripts/preprocess.py
+++ scripts/preprocess.py
@@ -1,155 +1,127 @@
+#!/usr/bin/env python
 # -*- coding: utf-8 -*-
-
-# MODIFIED from original file.
-# Options:
-#   --train_features - The parent directory of the training .npy features
-#   --val_features - The parent directory of the validation .npy features
-#   --test_features - The parent directory of the testing .npy features
-#   --train_annotations - The .json file of the training annotations
-#   --val_annotations - The .json file of the validation annotations
-#   --output_h5 - The output .h5 file containing the formatted features
-#   --output_json - The output .json file containing the vocabulary
-
 import argparse, json, os
 import numpy as np
 import h5py
 import codecs
-
+import string
+import glob
+import os
+from collections import defaultdict
 
 parser = argparse.ArgumentParser()
-#parser.add_argument('--input_txt', default='data/tiny-shakespeare.txt')
-#parser.add_argument('--output_h5', default='data/tiny-shakespeare.h5')
-#parser.add_argument('--output_json', default='data/tiny-shakespeare.json')
-#parser.add_argument('--val_frac', type=float, default=0.1)
-#parser.add_argument('--test_frac', type=float, default=0.1)
-parser.add_argument('--quiet', action='store_true')
-parser.add_argument('--encoding', default='utf-8')
-#---New args----
-parser.add_argument('--train_features', default = /data/coco/train2014)
-parser.add_argument('--val_features', default = /data/coco/val2014)
-parser.add_argument('--test_features', default = /data/coco/test2014)
-parser.add_argument('--train_annotations', \
-    default = /data/annotations/captions_train2014.json)
-parser.add_argument('--val_annotations', \
-    default = /data/annotations/captions_val2014.json)
-parser.add_argument('--output_h5', default='../data/coco.h5')
-parser.add_argument('--output_json', default='../data/coco.json')
-#---------------
+parser.add_argument('--train_json', default='/data/coco/annotations/captions_train2014.json')
+parser.add_argument('--val_json', default='/data/coco/annotations/captions_val2014.json')
+parser.add_argument('--test_dir', default='/data/coco/test2014')
+parser.add_argument('--train_dir', default='/data/coco/train2014')
+parser.add_argument('--val_dir', default='/data/coco/val2014')
+parser.add_argument('--output_h5', default='coco.h5')
+parser.add_argument('--output_json', default='coco_vocab.json')
 args = parser.parse_args()
 
-if __name__ == '__main__':
-  if args.encoding == 'bytes': args.encoding = None
+h5 = h5py.File(args.output_h5, "w")
 
-  # First go the file once to see how big it is and to build the vocab
-  token_to_idx = {}
-  total_size = 0
-  num_captions = {args.train_annotations : 0, args.val_annotations: 0}
-  max_caption = 0
-  annotations = [args.train_annotations, args.val_annotations]
-  for json_name in annotations:
-    # Open the .json file
-    with codecs.open(json_name, 'r', args.encoding) as f:
-        # Parse the contents
-        parsed_json = json.load(f)
+# Make the test dataset
+test_files = glob.glob(os.path.join(args.test_dir,"*_f1.npy"))
+num_test = len(test_files)
+test_names = h5.create_dataset('test_names', shape=(num_test,), dtype="S10")
 
-        # Get each caption character
-        caption = "" #TODO get the caption somehow
-        max_caption = max(max_caption, len(caption))
-        total_size += len(caption)
-        num_captions[json_name] += 1
-        for char in caption:
-            if char not in token_to_idx:
-                token_to_idx[char] = len(token_to_idx) + 1
-        
-  #total_size = 0
-  #with codecs.open(args.input_txt, 'r', args.encoding) as f:
-  #  for line in f:
-  #    total_size += len(line)
-  #    for char in line:
-  #      if char not in token_to_idx:
-  #        token_to_idx[char] = len(token_to_idx) + 1
+feat_len = None
+test_dset = None
+for i,fname in enumerate(test_files):
+  feats = np.load(fname).mean(axis=(1,2)).squeeze()
+  if feat_len is None:
+	feat_len = feats.shape[0]
+	test_dset = h5.create_dataset('test_feats', shape=(num_test,feat_len))
+  assert(len(feats.shape) == 1)
+  assert(feat_len == feats.shape[0])
+  test_dset[i] = feats
+  test_names[i] = fname
+  if i % 100 == 0:
+	print "Adding caption %d to test dataset" % (i)
 
-  # Now we can figure out the split sizes
-  #val_size = int(args.val_frac * total_size)
-  #test_size = int(args.test_frac * total_size)
-  #train_size = total_size - val_size - test_size
- 
-  if not args.quiet:
-    print 'Total vocabulary size: %d' % len(token_to_idx)
-    print 'Total tokens in file: %d' % total_size
-    #print '  Training size: %d' % train_size
-    #print '  Val size: %d' % val_size
-    #print '  Test size: %d' % test_size
+# Read the input captions
+trainf = open(args.train_json)
+valf = open(args.val_json)
+train_json = json.load(trainf)
+val_json = json.load(valf)
 
-  # Choose the datatype based on the vocabulary size
-  dtype = np.uint8
-  if len(token_to_idx) > 255:
-    dtype = np.uint32
-  if not args.quiet:
-    print 'Using dtype ', dtype
+captions = {'train':defaultdict(list), 'val':defaultdict(list)}
+token_to_idx = {}
+max_caption_length= -float('inf')
+num_capts=defaultdict(int)
 
-  # Just load data into memory ... we'll have to do something more clever
-  # for huge datasets but this should be fine for now
-  train = np.zeros(train_size, dtype=dtype)
-  val = np.zeros(val_size, dtype=dtype)
-  test = np.zeros(test_size, dtype=dtype)
-  splits = [train, val, test]
+# Add the inputs captions to dictionaries
+for name,js in [('train',train_json),('val',val_json)]:
+  for ann in js['annotations']:
+    im_id = ann['image_id']
+    caption = ann['caption']
+    caption = caption.encode('ascii','ignore').strip().lower()
+    caption = caption.translate(None,string.punctuation)
+    for word in caption.split():
+      if word not in token_to_idx:
+        token_to_idx[word] = len(token_to_idx)
+    max_caption_length = max(max_caption_length, len(caption))
+    captions[name][im_id].append([token_to_idx[word] for word in caption.split()])
+    num_capts[name] +=1
 
-  # Go through the files again and write data to the HDF5 file
-  feat_len = 0 # TODO read the length of a feature
-  num_test = 0 # TODO get the length of the testing set, or resize text_x on the fly
-  num_train = num_captions[args.train_annotations];
-  num_val = num_captions[args.val_annotations];
-  with h5py.File(args.output_h5, 'w') as f:
-    # Initialize the datasets
-    train_x = f.create_dataset('train_x', shape=(num_train, feat_len))
-    val_x = f.create_dataset('val_x', shape=(num_val, feat_len))
-    test_x = f.create_dataset('test_x', shape=(num_test, feat_len))
-    train_y = f.create_dataset('train_y', shape=(num_train, max_caption), dtype = dtype)
-    val_y = f.create_dataset('val', shape=(num_val, max_caption), dtype = dtype)
+print "Loaded %d training captions" % num_capts['train']
+print "Loaded %d validation captions" % num_capts['val']
+# Add start and null to the vocab
+start_tok = "<START>"
+end_tok = "<NULL>"
+token_to_idx[start_tok] = len(token_to_idx)
+token_to_idx[end_tok] = len(token_to_idx)
 
-    # Add each feature and caption
-    for json_name in annotations:
-        with codecs.open(json_name, 'r', args.encoding) as j:
-            parsed_json = json.load(j)
-            caption = "" #TODO get the caption somehow
-            # Update the y label
+if len(token_to_idx) > 2**32:
+  print "You fucked up, son"
+  exit(1)
 
-            # Get the x label according this file (JSON 'image_id')
-                
+# create the training and validation datasets
+# dataset will be N x max_caption_length
+num_train = num_capts['train']
+num_val = num_capts['val']
+train_feats = h5.create_dataset('train_feats', shape=(num_train,feat_len))
+train_names = h5.create_dataset('train_names', shape=(num_train,), dtype="S10")
+val_feats = h5.create_dataset('val_feats', shape=(num_val,feat_len))
+val_names = h5.create_dataset('val_names', shape=(num_val,), dtype="S10")
+train_captions = h5.create_dataset('train_captions', shape=(num_val,max_caption_length),dtype='i4')
+val_captions = h5.create_dataset('val_captions', shape=(num_val,max_caption_length),dtype='i4')
 
-  # Go through the file again and write data to numpy arrays
-  #split_idx, cur_idx = 0, 0
-  #with codecs.open(args.input_txt, 'r', args.encoding) as f:
-  #  for line in f:
-  #    for char in line:
-  #      splits[split_idx][cur_idx] = token_to_idx[char]
-  #      cur_idx += 1
-  #      if cur_idx == splits[split_idx].size:
-  #        split_idx += 1
-  #        cur_idx = 0
+# Add each caption to the datasets
+for dset_name,dset_capts in captions.iteritems():
+  curr_capt = 0
+  for im_id, im_captions in dset_capts.iteritems():
+    fname = "COCO_%s2014_%012d_f1.npy" % (dset_name, int(im_id))
+    if dset_name =="train":
+      fname = os.path.join(args.train_dir,fname)
+    elif dset_name == "val":
+      fname = os.path.join(args.val_dir,fname)
+    try:
+      feats = np.load(fname).mean(axis=(1,2))
+    except:
+      print "FUCK"
+      print fname
+      print np.load(fname).shape
+      exit(1)
+    for caption in im_captions:
+      # pad
+      caption += [token_to_idx[end_tok]]*(max_caption_length-len(caption))
+      if dset_name == 'train':
+        train_feats[curr_capt] = feats
+	train_names[curr_capt] = fname
+        train_captions[curr_capt] = np.array(caption,dtype=np.uint32)
+      if dset_name == 'val':
+        val_feats[curr_capt] = feats
+	val_names[curr_capt] = fname
+        val_captions[curr_capt] = np.array(caption,dtype=np.uint32)
+      curr_capt += 1
+      if curr_capt % 100 == 0:
+	print "Adding caption %d to dataset %s" % (curr_capt, dset_name)
 
-  # Write data to HDF5 file
-  #with h5py.File(args.output_h5, 'w') as f:
-  #  f.create_dataset('train', data=train)
-  #  f.create_dataset('val', data=val)
-  #  f.create_dataset('test', data=test)
+# Make the output JSON vocabulary
+idx_to_token = [(idx,token) for token,idx in token_to_idx.items()]
+json_data = { "idx_to_token":idx_to_token,"token_to_idx":token_to_idx}
 
-  # For 'bytes' encoding, replace non-ascii characters so the json dump
-  # doesn't crash
-  if args.encoding is None:
-    new_token_to_idx = {}
-    for token, idx in token_to_idx.iteritems():
-      if ord(token) > 127:
-        new_token_to_idx['[%d]' % ord(token)] = idx
-      else:
-        new_token_to_idx[token] = idx
-    token_to_idx = new_token_to_idx
-
-  # Dump a JSON file for the vocab
-  json_data = {
-    'token_to_idx': token_to_idx,
-    'idx_to_token': {v: k for k, v in token_to_idx.iteritems()},
-  }
-  with open(args.output_json, 'w') as f:
-    json.dump(json_data, f)
+with open(args.output_json, 'w') as f:
+  json.dump(json_data,f)
