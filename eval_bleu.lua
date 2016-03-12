ce881e347e0bae24ca44fc04e1805f1ce590fd2c
Blaine Rister <blaine@stanford.edu>
2016-03-12 12:23:19 +0000

    added script to graph bleu

--- eval_bleu.lua
+++ eval_bleu.lua
@@ -7,6 +7,7 @@ print 'Loading the torch model...'
 local cmd = torch.CmdLine()
 cmd:option('-h5', '/data/coco/coco_bgr.h5')
 cmd:option('-checkpoint','/data/checkpoints/64_1536_2_2500_10000.t7')
+cmd:option('-split', 'val')
 cmd:option('-device', 1)
 cmd:option('-batchSize', 512)
 local opt = cmd:parse(arg)
@@ -15,6 +16,6 @@ cutorch.setDevice(opt.device)
 local checkpoint = torch.load(opt.checkpoint)
 
 print 'Computing BLEU on the tiny dataset...'
-local score = bleu.getScore(checkpoint, opt.h5, 'val', 'cuda', opt.device, opt.batchSize)
+local score = bleu.getScore(checkpoint, opt.h5, opt.split, 'cuda', opt.device, opt.batchSize)
 print(score)
 
