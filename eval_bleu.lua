c2dc0024a70c9a48132a12746f396a3c89c2e00b
Blaine Rister <blaine@stanford.edu>
2016-03-12 05:22:34 +0000

    update eval script for new bleu.lua

--- eval_bleu.lua
+++ eval_bleu.lua
@@ -8,12 +8,13 @@ local cmd = torch.CmdLine()
 cmd:option('-h5', '/data/coco/coco_test.h5')
 cmd:option('-checkpoint','/data/checkpoints/64_1536_2_2500_10000.t7')
 cmd:option('-device', 1)
+cmd:option('-batchSize', 32)
 local opt = cmd:parse(arg)
 
 cutorch.setDevice(opt.device)
 local checkpoint = torch.load(opt.checkpoint)
 
 print 'Computing BLEU on the tiny dataset...'
-local score = bleu.getScore(checkpoint, opt.h5, 'val', 'cuda', opt.device)
+local score = bleu.getScore(checkpoint, opt.h5, 'val', 'cuda', opt.device, opt.batchSize)
 print(score)
 
